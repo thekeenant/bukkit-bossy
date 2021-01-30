becd0613c395d8625f50c16beda1128737c68d3f
Austin L Mayes <me@austinlm.me>
2021-01-29 23:14:13 -0600

    Keep the bar alive instead of respawning it constantly
    Signed-off-by: Austin L Mayes <me@austinlm.me>

--- src/main/java/com/keenant/bossy/Bossy.java
+++ src/main/java/com/keenant/bossy/Bossy.java
@@ -1,29 +1,28 @@
 package com.keenant.bossy;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.PacketType.Play.Server;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NonNull;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import javax.annotation.Nonnull;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Bossy {
     private static List<Plugin> plugins = new ArrayList<>();
@@ -48,8 +47,8 @@ public class Bossy {
             public void run() {
                 for (BossBar bar : bossBars.values()) {
                     if (bar.isSpawned()) {
-                        despawn(bar);
-                        spawn(bar, getDistantLocation(bar.getPlayer()));
+                        bar.setLocation(getDistantLocation(bar.getPlayer()));
+                        teleport(bar);
                     }
                 }
             }
@@ -97,9 +96,9 @@ public class Bossy {
         else
             bar.setText(text);
 
-        if (bar.isSpawned())
-            despawn(bar);
-        spawn(bar, getDistantLocation(player));
+        if (bar.isSpawned()) {
+            render(bar);
+        } else spawn(bar, getDistantLocation(player));
     }
 
     /**
@@ -116,9 +115,9 @@ public class Bossy {
         else
             bar.setHealth(convertToHealth(percent));
 
-        if (bar.isSpawned())
-            despawn(bar);
-        spawn(bar, getDistantLocation(player));
+        if (bar.isSpawned()) {
+            render(bar);
+        } else spawn(bar, getDistantLocation(player));
     }
 
     /**
@@ -160,7 +159,41 @@ public class Bossy {
     }
 
     private Location getDistantLocation(Player player) {
-        return player.getEyeLocation().add(player.getEyeLocation().getDirection().normalize().multiply(23));
+        Location location = player.getEyeLocation().clone();
+        location.setPitch(location.getPitch() - 21);
+
+        return location.add(location.getDirection().normalize().multiply(50));
+    }
+
+    private void render(BossBar bar) {
+        PacketContainer metadataPacket = new PacketContainer(Server.ENTITY_METADATA);
+        metadataPacket.getModifier().write(0, CUSTOM_ID);
+        metadataPacket.getWatchableCollectionModifier().write(0, bar.getDataWatcher().getWatchableObjects());
+
+        try {
+            ProtocolLibrary.getProtocolManager().sendServerPacket(bar.getPlayer(), metadataPacket, false);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void teleport(BossBar bar) {
+        PacketContainer teleportPacket = new PacketContainer(Server.ENTITY_TELEPORT);
+        StructureModifier<Object> teleportPacketModifier = teleportPacket.getModifier();
+
+        teleportPacketModifier.write(0, CUSTOM_ID);
+        teleportPacketModifier.write(1,  (bar.getLocation().getBlockX() * 32));
+        teleportPacketModifier.write(2,  (bar.getLocation().getBlockY() * 32));
+        teleportPacketModifier.write(3,  (bar.getLocation().getBlockZ() * 32));
+        teleportPacketModifier.write(4, (byte) (bar.getLocation().getYaw() * 256 / 360));
+        teleportPacketModifier.write(5, (byte) (bar.getLocation().getPitch() * 256 / 360));
+        teleportPacketModifier.write(6, true);
+
+        try {
+            ProtocolLibrary.getProtocolManager().sendServerPacket(bar.getPlayer(), teleportPacket, false);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     private void spawn(BossBar bar, Location location) {
@@ -171,9 +204,10 @@ public class Bossy {
         StructureModifier<Object> spawnPacketModifier = spawnPacket.getModifier();
         spawnPacketModifier.write(0, CUSTOM_ID);
         spawnPacketModifier.write(1, (byte) 64);
-        spawnPacketModifier.write(2, location.getBlockX() * 32);
-        spawnPacketModifier.write(3, location.getBlockY() * 32);
-        spawnPacketModifier.write(4, location.getBlockZ() * 32);
+        spawnPacketModifier.write(2, bar.getLocation().getBlockX() * 32);
+        spawnPacketModifier.write(3, bar.getLocation().getBlockY() * 32);
+        spawnPacketModifier.write(4, bar.getLocation().getBlockZ() * 32);
+
         spawnPacket.getDataWatcherModifier().write(0, bar.getDataWatcher());
 
         try {
@@ -214,11 +248,15 @@ public class Bossy {
 
         private WrappedDataWatcher getDataWatcher() {
             WrappedDataWatcher watcher = new WrappedDataWatcher();
-            watcher.setObject(0, (byte) 32);
-            watcher.setObject(2, this.text);
+            watcher.setObject(0, (byte) 0x20); // invisible
+            watcher.setObject(1, (short) 300); // air ticks
+            watcher.setObject(2, this.text); // name
+            watcher.setObject(3, (byte) 1); // show name
             watcher.setObject(6, (float) this.health, true); // Set health
-            watcher.setObject(10, this.text);
-            watcher.setObject(20, 881);
+            watcher.setObject(7, (int) Color.BLACK.asRGB()); // potion color
+            watcher.setObject(8, (byte) 0); // potion ambient
+            watcher.setObject(15, (byte) 1); // No AI
+            watcher.setObject(20, 819); // Invulnerable
             return watcher;
         }
     }
